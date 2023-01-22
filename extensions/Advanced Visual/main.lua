05383d07ef067562d54787424bc658e6f136042b
DereBoi <81463152+lolhahaXD@users.noreply.github.com>
2023-01-22 12:03:43 +0100

    Delete extensions/Advanced Visual directory

--- extensions/Advanced Visual/main.lua
+++
@@ -1,9 +0,0 @@
-function ExtensionLoaded()
-    Project.CopyExtensionFile("AdvancedVisual.lua", "scripts/#AdvancedVisual.lua")
-    Project.CopyExtensionFile("AdvVisuals_Lists.lua", "scripts/#AdvVisuals_Lists.lua")
-end
-
-function ExtensionUnloaded()
-    Project.RemoveFile("scripts/#AdvancedVisual.lua")
-    Project.RemoveFile("scripts/#AdvVisuals_Lists.lua")
-end
